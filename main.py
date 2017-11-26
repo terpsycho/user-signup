def296b74f399772244d9b77d995106fc8d9516d
terpsycho <arcbrf@mail.umkc.edu>
2017-11-26 11:04:33 -0600

    Begining of the forms and their labels

---
+++ main.py
@@ -0,0 +1,45 @@
+form = """
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+        </style>
+    </head>
+    <body>
+      <h1>Signup</h1>
+      <form method="post">
+      <label for= >Username</label>
+      <input name="username" type="text" value>
+      <label for= >Password</label>
+      <input name="password" type="password" value>
+      <label for= >Verify Password</label>
+      <input name="pwverify" type="password" value>
+      <label for= >Email Address</label>
+      <input name="email" type="text" value>
+      <sumbit buttoN>
+
+
+
+
+    </body>
+</html>
+
+
+
+
+
+"""
