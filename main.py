2faf30e3b7401d53ed6941e30361411eb7700e9a
terpsycho <arcbrf@mail.umkc.edu>
2017-12-07 20:00:42 -0600

    Almost done but still working on email validation and need to put the forms into the templates once that works

--- main.py
+++ main.py
@@ -1,62 +1,228 @@
 from flask import Flask, request, redirect
-import os 
+import cgi
+import os
 import jinja2
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-  
-@app.route('/')
-def info():
-    template = jinja_env.get_template('signup_form.html')
+@app.route("/")
+def index():
+    template = jinja_env.get_template('hello_form.html')
     return template.render()
-
-
-@app.route('/info', methods=['POST'])
-def validate_info():
-
-    username = request.form['username']
-    password = request.form['password']
-    pwverify = request.form['pwverify']
-    email = request.form['email']
-
-    username_error = ''
-    password_error = ''
-    pwverify_error = ''
-    email_error = ''
-
-    if not is_valid_username(username):
-        username_error = "Not a valid username."
-    
-    #if not is_valid_username(password):
-        #password_error = 'Not a valid password.'
-    
-    #if not is_valid_username(pwverify):
-        #pwverify_error = "Passwords do not match."
-
-    #if not is_valid_username(email):
-        #email_error = "Not a valid email."
-
-
-    def is_valid_username(name):
-       try: 
-            name is "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"
-            return True
-
-       except ValueError:
-            return False
 
 @app.route("/hello", methods=['POST'])
 def hello():
-    username=''
-    firstname = request.form['username']
-    return '<h1> Hello, ' + firstname + '</h1>'
+    first_name = request.form['first_name']
+    template = jinja_env.get_template('hello_greeting.html')
+    return template.render(name=first_name)
 
-#@app.route("/")
-#def index():
-    #return form
+
+form = """
+    <style>
+        .error {{ color: red; }}
+    </style>
+    <h1>Signup</h1>
+    <form method='POST'>
+
+        <label>Username 
+            <input name="username" type="text" value='{username}' />
+        </label>
+        <span class="error">{username_error}</span><br>
+
+        <label>Password
+            <input name="password" type="password" value='{password}' />
+        </label>
+        <span class="error">{password_error}</span><br>
+
+        <label>Verify Password
+            <input name="verify" type="password" value='{verify}' />
+        </label>
+        <span class="error">{verify_error}</span><br>
+
+        <label>Email
+            <input name="email" type="text" value='{email}' />
+        </label>
+        <span class="error">{email_error}</span><br>
+
+
+        <input type="submit" value="Signup!" />
+    </form>
+    """
+
+@app.route('/signup')
+def display_form():
+    return form.format(username='', username_error='',
+        password='', password_error='', verify='', verify_error='',
+        email='', email_error='')
+
+
+def is_valid(data):
+    #valid_characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+    valid_data = str(data)
+    if valid_data.isalnum():
+        return True
+    else:
+        return False
+
+#Need to fix email validation!!!       
+def valid_email(email):
+    email = request.form['email']
+    ats = email.count('@')
+    dots = email.count('.')
+    if ats == 0 and dots == 0:
+        return True
+    else: 
+        return False
+
+@app.route('/signup', methods=['POST'])
+def validate_data():
+
+    username = request.form['username']
+    password = request.form['password']
+    verify = request.form['verify']
+    email = request.form['email']
+
+
+    username_error = ''
+    password_error = ''
+    verify_error = ''
+    email_error = ''
+
+    #str(username)
+    #str(password)
+    #str(verify)
+    #str(email)
+
+#Username Errors
+    if len(username) <= -1:
+        username_error= "Must type a username."
+        username= ''
+        password= ''
+        verify= ''
+        email= email
+
+    if len(username) <= 2:
+        username_error = 'Length of username must be between 3 and 20 characters.'
+        username= ''
+        password= ''
+        verify= ''
+        email= email
+
+    if len(username) > 19:
+        username_error = 'Length of username must be between 3 and 20 characters.'
+        username= ''
+        password= ''
+        verify= ''
+        email= email
+
+    if not is_valid(username):
+        username_error = 'Not a valid username.'
+        username= ''
+        password= ''
+        verify= ''
+        email= email
+
+
+
+#Password Errors
+    if len(username) <= -1:
+        password_error= "Must type a password."
+        username= username
+        password= ''
+        verify= ''
+        email= email    
+
+    if len(password) <= 2:
+        password_error = 'Length of password must be between 3 and 20 characters.'
+        username= username
+        password= ''
+        verify= ''
+        email= email   
+
+    if len(password) > 19 :
+        password_error = 'Length of password must be between 3 and 20 characters.'
+        username= username
+        password= ''
+        verify= ''
+        email= email   
+
+    if not is_valid(password):
+        password_error = 'Not a valid password.'
+        username= username
+        password= ''
+        verify= ''
+        email= email
+
+
+
+#Verify Password Errors
+    if len(verify) <= -1:
+        verify_error= "Must verify password."
+        username= username
+        password= ''
+        verify= ''
+        email= email
+
+    if not verify == password:
+        verify_error = 'Passwords do not match.'
+        username= username
+        password= ''
+        verify= ''
+        email= email
+
+
+#Email Errors
+    if len(email) <= -1:
+        username= username
+        password= password
+        verify= verify
+        email= ''
+
+    elif not valid_email(email):
+        email_error= "Not a valid email address."
+        username= username
+        password= ''
+        verify= ''
+        email= email
+
+    #elif not is_valid(email):
+        #email_error= "Not a valid email addressmmmm."
+        #username= username
+        #password= ''
+        #verify= ''
+        #email= email
+    
+    else:
+        username= username
+        password= password
+        verify= verify
+        email= email 
+
+
+#General Errors
+    if not password_error and not username_error and not verify_error and not email_error:
+        user = str(username)
+        return redirect('/hellouser?user={0}'.format(user))
+    else:
+        return form.format(username_error=username_error,
+            password_error=password_error, verify_error= verify_error,
+            email_error=email_error,
+            username=username,
+            password=password,
+            verify=verify,
+            email=email)
+
+
+@app.route('/hellouser')
+def valid_user():
+    user = request.args.get('user')
+    return '<h1>Hello, {0}.</h1>'.format(user)
+
+
+
 
 app.run()
